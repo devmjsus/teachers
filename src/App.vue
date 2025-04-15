204d7c97d06315c633d42c07bf4c20b287ead66e
Jesus <jesdmont@outlook.com>
2025-04-15 17:43:26 -0500

    add packages, initialize

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+  import { TeacherForm } from './components/TeacherForm.vue';
+</script>
+
+<template>
+<TeacherForm />
+
+<h1>Hello World</h1>
+</template>
+
+<style scoped>
+
+</style>
