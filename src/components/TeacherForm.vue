8f0912cf543c78e44b1d5a218100b9248ffd5e32
Jesus <jesdmont@outlook.com>
2025-04-15 21:34:13 -0500

    feat: enhance TeacherForm with dynamic input handling and display of teacher list

--- src/components/TeacherForm.vue
+++ src/components/TeacherForm.vue
@@ -1,19 +1,85 @@
 <template>
 	<section>
 		<h3>Add teacher</h3>
-		<div><label for="">Name:</label> <input type="text" /></div>
-		<div><label for="">Surname:</label> <input type="text" /></div>
-		<div><label for="">CC/TI:</label> <input type="text" /></div>
-		<div><label for="">Subjects:</label> <input type="text" /></div>
-		<input type="checkbox" /> Documentation
-		<button>Add</button>
+		<div><label for="">Name:</label> <input type="text" v-model="teacher.teacherName" /></div>
+		<div><label for="">surname:</label> <input type="text" v-model="teacher.surname" /></div>
+		<div><label for="">CC/TI:</label> <input type="text" v-model="teacher.cc" /></div>
+		<div><label for="">Subjects:</label> <input type="text" v-model="subject" /></div>
+		<input type="checkbox" v-model="teacher.doc" /> Documentation <button @click="handleSubject()">Add</button>
+		<div>
+			<ul>
+				<li v-for="(sub, index) in teacher.subjects" :key="index">{{ sub }}</li>
+			</ul>
+		</div>
+		<button @click="handleAddTeacher()">Add teacher</button>
 	</section>
-
+	
 	<section>
 		<h3>List of teachers</h3>
+		<table>
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Surname</th>
+					<th>CC/TI</th>
+					<th>Subjects</th>
+					<th>Documentation</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr v-for="(t, index) in teachers" :key="index">
+					<td>{{ t.teacherName }}</td>
+					<td>{{ t.surname }}</td>
+					<td>{{ t.cc }}</td>
+					<td>{{ t.subjects.join(', ') }}</td>
+					<td>{{ t.doc ? 'Yes' : 'No' }}</td>
+					<td v-if="t.doc">Entregada</td>
+					<td v-else>No Entregada</td>
+				</tr>
+			</tbody>
+		</table>
 	</section>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref, type Ref } from 'vue';
+interface ITeacher {
+	teacherName: string;
+	surname: string;
+	cc: string;
+	subjects: Array<string>;
+		doc: boolean;
+	}
+	const teacher: Ref<ITeacher> = ref({
+		teacherName: '',
+		surname: '',
+		cc: '',
+		subjects: [],
+		doc: false,
+	});
+	
+	const teachers: Ref<Array<ITeacher>> = ref([]);
+		
+		const subject: Ref<string> = ref('');
+			
+			const handleSubject = () => {
+				teacher.value.subjects.push(subject.value);
+				subject.value = ''; // Clear the input after adding
+			}
+			
+			const handleAddTeacher = () => {
+				teachers.value.push(teacher.value);
+				teacher.value = {
+					teacherName: '',
+					surname: '',
+					cc: '',
+					subjects: [],
+					doc: false
+				}; // Reset the form after adding
+			}
+			
+</script>
 
-<style scoped></style>
+<style scoped>
+
+</style>
